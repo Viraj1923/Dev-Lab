4439077dc337046b89b29306131d452e1f558566
Viraj Mulik <virajmulik2304@gmail.com>
2026-08-23 21:56:34 +0530

    feat: complete FastAPI CRUD with PostgreSQL

--- backend/fastapi/02-project-structure/routers/users.py
+++ backend/fastapi/02-project-structure/routers/users.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter,Depends,HTTPException, status
-from schemas.user import UserCreate,UserResponse
+from schemas.user import UserCreate, UserResponse, UserUpdate
 from sqlalchemy.orm import Session
 from models.user import User
 from database import get_db
@@ -43,3 +43,33 @@ def get_user(user_id: int,db:Session=Depends(get_db)):
                 detail="User Not Found"
         )
     return user_data
+
+@router.put("/{user_id}",response_model=UserResponse)
+def update_user(user_id:int,user:UserUpdate,db:Session=Depends(get_db)):
+    user_data = db.query(User).filter(User.id == user_id).first()
+    if not user_data:
+                raise HTTPException(
+                    status_code=404, 
+                    detail="User Not Found"
+        )
+    update_data = user.model_dump(exclude_unset=True)
+    for key, value in update_data.items():
+        setattr(user_data, key, value)
+
+    db.commit()
+    db.refresh(user_data)
+    return user_data
+
+@router.delete("/{user_id}")
+def delete_user(user_id,db:Session=Depends(get_db)):
+    user_data=db.query(User).filter(User.id==user_id).first()
+    if not user_data:
+                    raise HTTPException(
+                        status_code=404, 
+                        detail="User Not Found"
+        )
+    db.delete(user_data);
+    db.commit()
+    return {"message": "User deleted successfully"}
+    
+      
--- backend/fastapi/02-project-structure/schemas/user.py
+++ backend/fastapi/02-project-structure/schemas/user.py
@@ -11,3 +11,8 @@ class UserResponse(BaseModel):
     name: str
     age: int
     email: str
+
+class UserUpdate(BaseModel):
+    name: str | None = None
+    age: int | None = None
+    email: str | None = None
