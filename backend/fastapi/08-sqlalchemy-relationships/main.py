9f062469091ee6b9ed076379c71c8e206b4aab8d
Viraj Mulik <virajmulik2304@gmail.com>
2026-08-28 16:47:21 +0530

    Explore SQLAlchemy loading strategies

--- backend/fastapi/08-sqlalchemy-relationships/main.py
+++ backend/fastapi/08-sqlalchemy-relationships/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, selectinload
 
 from database import Base, engine, SessionLocal
 from models.user import User
@@ -110,6 +110,85 @@ def test_courses():
             for course in user.courses
         ]
     }
+
+    db.close()
+
+    return result
+
+@app.get("/test-course-users")
+def test_course_users():
+    db: Session = SessionLocal()
+
+    course = db.query(Course).filter(Course.name == "FastAPI").first()
+
+    result = {
+        "course_id": course.id,
+        "course": course.name,
+        "users": [
+            {
+                "id": user.id,
+                "name": user.name
+            }
+            for user in course.users
+        ]
+    }
+
+    db.close()
+
+    return result
+
+
+@app.get("/test-lazy/{user_id}")
+def test_lazy(user_id: int):
+    db: Session = SessionLocal()
+
+    print("1. Querying user")
+
+    user = db.query(User).filter(User.id == user_id).first()
+
+    print("2. User loaded")
+
+    print("3. Accessing posts")
+
+    posts = user.posts
+
+    print("4. Posts loaded")
+
+    result = {
+        "user": user.name,
+        "posts": [
+            {
+                "id": post.id,
+                "title": post.title
+            }
+            for post in posts
+        ]
+    }
+
+    db.close()
+
+    return result
+
+@app.get("/test-n-plus-one-fixed")
+def test_n_plus_one():
+    db: Session = SessionLocal()
+
+    users = (
+        db.query(User)
+        .options(selectinload(User.posts))
+        .all()
+    )
+
+    result = []
+
+    for user in users:
+        result.append({
+            "user": user.name,
+            "posts": [
+                post.title
+                for post in user.posts
+            ]
+        })
 
     db.close()
 
